cf850ef4cfd5572ab3ebe71290270f778ac3e858
WassimBelghache <wassim.belghache@ucalgary.ca>
2024-07-28 21:39:04 -0600

    reverse

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let saveEl = document.getElementById("save-el");
-    let countEl = document.getElementById("count-el");
-    let count = 0;
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
-    function increment() {
-        count += 1;
-        countEl.textContent = count;
-    }
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
 
-    function save() {
-        let countStr = count + " - ";
-        saveEl.textContent += countStr;
-        countEl.textContent = 0;
-        count = 0;
-    }
-
-    // Attach the functions to the window object to ensure they are in the global scope
-    window.increment = increment;
-    window.save = save;
-});
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
